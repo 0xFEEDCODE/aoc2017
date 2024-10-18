a7d56b453839196766374f4e5cb154b0d3effa67
some00000001 <samo.seman96@gmail.com>
2024-10-18 09:22:18 +0200

    day17 pt2

--- aoc_2017/day17.fs
+++ aoc_2017/day17.fs
@@ -1,6 +1,5 @@
 module aoc_2017.day17
 
-open System
 open System.Collections.Generic
 open System.Linq
 
@@ -10,77 +9,85 @@ let nSpins = 50000000
 [<Literal>]
 let inp = 337
 
+
 let solve () =
 
     let ll = LinkedList()
     let head = ll.AddFirst 0
     
-    //1,2,4,7,16,33,146,483
-    //1,2,3,9,17,113
-    let mutable px = 0
-    let mutable pc = 0
-
-    let r =
-        seq { 0..10000000 }
-        |> Seq.scan
-            (fun (currNode: LinkedListNode<int>, currNodePos) x ->
-                let insertPos = (inp + currNodePos) % ll.Count
-                let offset = (insertPos - currNodePos) 
-
-                let insertPoint =
-                    if offset = 0 then
-                        currNode
-                    else
-                        Enumerable.Range(0, abs offset)
-                        |> Seq.fold
-                            (fun (node: LinkedListNode<int>) _ ->
-                                if offset > 0 then
-                                    if node.Next <> null then node.Next else ll.First
-                                else
-                                    if node.Previous <> null then node.Previous else ll.Last)
-                            currNode
-                            
-                let valueToInsert = x + 1
-                let insertedNodePos = insertPos + 1
-                let insertedNode = ll.AddAfter(insertPoint, valueToInsert)
-                
-                pc <- pc+1
-                if (insertedNodePos = 1) then
-                    printfn $"IA: {pc}, V: {valueToInsert}"
-                    pc <- 0
-                    
-                
-                (*
-                if(ll.Count > 1) then
-                    if((ll.Count - ll.First.Next.Value) = 1) then
-                        printfn $"%A{(ll.Count, ll.Last.Value)}"
-                        *)
-                
-                (*
-                printfn "____"
-                let mutable it = ll.First
-
-                let mutable c = 0
-                pc <- pc+1
-                while (it <> null) do
-                    printf "%A " it.Value
-                    c <- c+1
-                    it <- it.Next
-                    (*
-                    if(c = 1) then
-                        if px <> it.Value then
-                            px <- it.Value
-                            //printfn $"Change %A{(pc, ll.First.Next.Value, ll.Count)}"
-                            pc <- 0
-                            *)
-
-                *)
-                (insertedNode, insertedNodePos))
-            (head, 0)
-        |> Seq.toArray
-
-    //printfn "%A" (r |> Seq.iter(fun (x,y) -> printfn "%A" x.Value))
+    let mutable llCount = ll.Count
     
-    printfn "%A" (ll.First.Next.Value)
+    let insertNode2 (valueToInsert: int) (currNodePos : int) =
+        let insertPos = (inp + currNodePos) % llCount
+        let insertedNodePos = insertPos + 1
+        
+        llCount <- llCount + 1
+        
+        if insertedNodePos = 1 then
+            let insertedNode = ll.AddAfter(head, valueToInsert)
+            (insertedNode, insertedNodePos)
+        else
+            (head, insertedNodePos)
+    
+    let insertNode (valueToInsert: int) (currNode : LinkedListNode<int>) (currNodePos : int) =
+        let insertPos = (inp + currNodePos) % ll.Count
+        
+        let offsetFromCurrNode = (currNode, (insertPos - currNodePos))
+        let offsetStart = (ll.First, insertPos)
+        let offsetEnd = (ll.Last, insertPos - ll.Count)
+        
+        let closestOffsetPivotNode, closestOffsetDistance = [offsetFromCurrNode; offsetStart; offsetEnd] |> Seq.minBy (fun x -> abs (snd x))
+        
+        let insertPoint =
+            if closestOffsetDistance = 0 then
+                closestOffsetPivotNode
+            else
+                Enumerable.Range(0, abs closestOffsetDistance)
+                |> Seq.fold
+                    (fun (node: LinkedListNode<int>) _ ->
+                        if closestOffsetDistance > 0 then
+                            if node.Next <> null then node.Next else ll.First
+                        else
+                            if node.Previous <> null then node.Previous else ll.Last)
+                    closestOffsetPivotNode
+                    
+        let insertedNode = ll.AddAfter(insertPoint, valueToInsert)
+        let insertedNodePos = insertPos + 1
+            
+        (insertedNode, insertedNodePos)
+        
+    let mutable pc = 0
+    
+    let ans1 =
+        (seq { 0..(2017-1) }
+            |> Seq.fold(fun (currNode: LinkedListNode<int>, currNodePos) x -> insertNode (x+1) currNode currNodePos) (head, 0)
+            |> fst
+        ).Next.Value
+        
+    
+    let ll = LinkedList()
+    let head = ll.AddFirst 0
+    
+    let mutable llCount = ll.Count
+    
+    // Inserts node only if the node position is 1
+    let insertNodePart2 (valueToInsert: int) (currNode : LinkedListNode<int>) (currNodePos : int) =
+        let insertPos = (inp + currNodePos) % llCount
+        let insertedNodePos = insertPos + 1
+        
+        llCount <- llCount + 1
+        
+        if insertedNodePos = 1 then
+            let insertedNode = ll.AddAfter(currNode, valueToInsert)
+            (insertedNode, insertedNodePos)
+        else
+            (currNode, insertedNodePos)
+    
+    let ans2 =
+        (seq { 0..(50000000-1) }
+        |> Seq.fold(fun (currNode: LinkedListNode<int>, currNodePos) x -> (insertNodePart2 (x+1) currNode currNodePos)) (head, 0)
+        |> fst).Value
+        
+    printfn $"%A{ans1}, %A{ans2}"
 
     ()
